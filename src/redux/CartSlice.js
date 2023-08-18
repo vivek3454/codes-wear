3cc0d07fe707700542202acf67216b78b7f1697d
Vivek <vivekparde2411@gmail.com>
2023-08-18 18:59:26 +0530

    created redux cartSlice

--- src/redux/CartSlice.js
+++ src/redux/CartSlice.js
@@ -2,18 +2,32 @@
 
 import {createSlice} from '@reduxjs/toolkit';
 
+
 const cartSlice = createSlice({
     name: 'cart',
     initialState: [],
     reducers: {
         add(state, action){
-            state.push(action.payload)
+            
+            // if (state.find((item)=> item.id === action.payload.id)) {
+            //     let a;
+            //     a = state.find((item)=> item.id === action.payload.id);
+            //     a.qty = a.qty + 1;
+            //     state = state.filter((item)=> item.id !== action.payload.id);
+            //     return [a];
+            // }
+            // else{
+                state.push(action.payload);
+            // }
         },
         remove(state, action){
             return state.filter((item)=> item.id !== action.payload);
+        },
+        clear(state){
+            return state = [];
         }
     }
 })
 
-export const {add,remove} = cartSlice.actions;
+export const {add, remove, clear} = cartSlice.actions;
 export default cartSlice.reducer;
