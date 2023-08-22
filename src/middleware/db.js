cc65c50bb377b15949bea7c31cb5bd3df35c508e
Vivek <vivekparde2411@gmail.com>
2023-08-22 20:07:22 +0530

    database connection

---
+++ src/middleware/db.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const connectToDb = async () => {
+    try {
+
+        if (mongoose.connection.readyState === 0) {
+            await mongoose.connect(process.env.MONGOOSE_URI);
+            console.log('connected to db');
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export default connectToDb;
