709c18990bccdb37ccd3cb72682e24b0b615c659
Vivek <vivekparde2411@gmail.com>
2023-08-25 19:57:07 +0530

    created getHoodies api end point

---
+++ src/app/api/getHoodies/route.js
@@ -0,0 +1,36 @@
+import connectToDb from "@/middleware/db";
+import Product from "@/models/Product";
+import { NextResponse } from "next/server";
+export async function GET(Request) {
+    // let color = ['blue', 'black', 'green', 'orange']
+    // let size = ['L', 'XL', 'M', 'S', 'XXL']
+    try {
+        await connectToDb();
+        const products = await Product.find({category: 'hoodies'});
+        let hoodies = {};
+        for (const product of products) {
+            if (product.title in hoodies) {
+                if (!hoodies[product.title].color.includes(product.color) && product.availableQty > 0) {
+                    hoodies[product.title].color.push(product.color);
+                }
+                if (!hoodies[product.title].size.includes(product.size) && product.availableQty > 0) {
+                    hoodies[product.title].size.push(product.size);
+                }
+            }
+            else {
+                if (product.availableQty > 0) {
+                    hoodies[product.title] = JSON.parse(JSON.stringify(product));
+                }
+                if (product.availableQty > 0) {
+                    hoodies[product.title].color = [product.color];
+                    hoodies[product.title].size = [product.size];
+                }
+            }
+        }
+        return NextResponse.json({ hoodies }, { status: 200 });
+
+    } catch (error) {
+        return NextResponse.json({ message: error.message }, { status: 500 });
+
+    }
+}
