5a73196a35b4dc95206c92b6a2218cb4e8d32bb4
Vivek <vivekparde2411@gmail.com>
2023-08-25 19:57:53 +0530

    created getMugs api end point

---
+++ src/app/api/getMugs/route.js
@@ -0,0 +1,36 @@
+import connectToDb from "@/middleware/db";
+import Product from "@/models/Product";
+import { NextResponse } from "next/server";
+export async function GET(Request) {
+    // let color = ['blue', 'black', 'green', 'orange']
+    // let size = ['L', 'XL', 'M', 'S', 'XXL']
+    try {
+        await connectToDb();
+        const products = await Product.find({category: 'mugs'});
+        let mugs = {};
+        for (const product of products) {
+            if (product.title in mugs) {
+                if (!mugs[product.title].color.includes(product.color) && product.availableQty > 0) {
+                    mugs[product.title].color.push(product.color);
+                }
+                if (!mugs[product.title].size.includes(product.size) && product.availableQty > 0) {
+                    mugs[product.title].size.push(product.size);
+                }
+            }
+            else {
+                if (product.availableQty > 0) {
+                    mugs[product.title] = JSON.parse(JSON.stringify(product));
+                }
+                if (product.availableQty > 0) {
+                    mugs[product.title].color = [product.color];
+                    mugs[product.title].size = [product.size];
+                }
+            }
+        }
+        return NextResponse.json({ mugs }, { status: 200 });
+
+    } catch (error) {
+        return NextResponse.json({ message: error.message }, { status: 500 });
+
+    }
+}
