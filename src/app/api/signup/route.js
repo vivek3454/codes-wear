bceb80a4246a35ad40c47d242b571781a9f0d293
Vivek <vivekparde2411@gmail.com>
2023-08-28 18:21:34 +0530

    encrypted password

--- src/app/api/signup/route.js
+++ src/app/api/signup/route.js
@@ -1,11 +1,16 @@
 import connectToDb from "@/middleware/db";
 import User from "@/models/User";
 import { NextResponse } from "next/server";
+import CryptoJS from "crypto-js";
 
 export async function POST(Request) {
     // const data = await Request.json();
     const {name,email, password} = await Request.json();
     await connectToDb();
-    const user = User.create({name, email,password });
-    return NextResponse.json({success: true, user: data});
+    const userExist = await User.findOne({email});
+    if (userExist) {
+        return NextResponse.json({success: false, message: 'email already exist'});
+    }
+    const user = User.create({name, email,password: CryptoJS.AES.encrypt(password, 'secret123').toString() });
+    return NextResponse.json({success: true, user: {name:user.name,email: user.email}});
 }
