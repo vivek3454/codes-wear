d040bc7ff39d4f6544e5926251a6903d58255c17
Vivek <vivekparde2411@gmail.com>
2023-08-30 21:15:02 +0530

    added logout function

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,10 +3,13 @@ import Navbar from '@/components/Navbar'
 import './globals.css'
 import { Inter } from 'next/font/google'
 import Footer from '@/components/Footer'
-import { useState } from 'react'
+import { Suspense, useState } from 'react'
 import AppProvider from '@/redux/AppProvider'
 import { ToastContainer, toast } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
+import { useEffect } from 'react'
+import { usePathname } from 'next/navigation'
+
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,16 +19,33 @@ export const metadata = {
 }
 
 export default function RootLayout({ children }) {
-  const [cart, setCart] = useState([])
+  const path = usePathname();
+  const [user, setUser] = useState({ value: null });
+  const [key, setKey] = useState(0);
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      setUser({ value: token });
+      setKey(Math.random());
+    }
+  }, [path])
+
+
+  const handleLogout = () => {
+    localStorage.removeItem('token');
+    setUser({ value: null })
+    setKey(Math.random());
+  }
+
   return (
     <html lang="en">
       <body className={inter.className}>
         <AppProvider>
-          <Navbar />
+          <Navbar user={user} key={key} handleLogout={handleLogout} />
           {children}
           <Footer />
         </AppProvider>
-        <ToastContainer/>
+        <ToastContainer />
       </body>
     </html>
   )
