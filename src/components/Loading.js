4bb9df80eb51f6d05699f04d4b95a8da1be4a5fe
Vivek <vivekparde2411@gmail.com>
2023-09-02 19:09:07 +0530

    used next js Image component

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,9 +1,10 @@
+import Image from 'next/image'
 import React from 'react'
 
 const Loading = () => {
     return (
         <div className='h-[70vh] text-2xl text-[#525252] font-semibold flex justify-center items-center'>
-            <img src="Gear.svg" alt="gear" width={35} height={35} />
+            <Image src='/Gear.svg' alt="gear" width={35} height={35} />
             Loading
         </div>
     )
