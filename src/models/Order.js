e111f6d2ae5ce44565b226835ec7d3403d8fb808
Vivek <vivekparde2411@gmail.com>
2023-09-03 22:15:13 +0530

    updated order schema for razorpay

--- src/models/Order.js
+++ src/models/Order.js
@@ -1,12 +1,26 @@
 import mongoose from 'mongoose';
 
 const orderSchema = new mongoose.Schema({
-    userId: {type: String, required: true},
-    products: [{productId: String, quantity: { type: Number, default: 1 }}],
-    address: {type: String,required: true},
-    amount: {type: Number,required: true},
-    status: {type: String,default: 'Pending' ,required: true},
-},{timestamps: true});
+    email: { type: String, required: true },
+    products: { type: Array },
+    paymentInfo: {
+        razorpay_payment_id: {
+            type: String,
+
+        },
+        razorpay_subscription_id: {
+            type: String,
+
+        },
+        razorpay_signature: {
+            type: String,
+
+        }
+    },
+    address: { type: String, required: true },
+    checkoutAmount: { type: Number, required: true },
+    status: { type: String, default: 'Pending', required: true },
+}, { timestamps: true });
 
 mongoose.models = {}
 export default mongoose.model('Order', orderSchema)
