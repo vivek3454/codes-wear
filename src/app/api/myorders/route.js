3415573cf6ae967f926f0b82c9aee45eec708f23
Vivek <vivekparde2411@gmail.com>
2023-09-05 17:42:00 +0530

    created myorders route and used in in orders page

---
+++ src/app/api/myorders/route.js
@@ -0,0 +1,21 @@
+import connectToDb from "@/middleware/db";
+import Order from "@/models/Order";
+import { NextResponse } from "next/server";
+import {verify} from "jsonwebtoken";
+import { cookies } from "next/headers";
+
+
+export async function POST(Request) {
+    try {
+        const token = cookies().get('codesWearJwt');
+        if (!token) {   
+            return NextResponse.json({ success: false, message: 'Not authorized' });
+        }
+        await connectToDb();
+        const data = verify(token?.value, process.env.JWT_SECRET);
+        let orders = await Order.find({email: data.email});
+        return NextResponse.json({ success: true, orders });
+    } catch (error) {
+        return NextResponse.json({ success: false, message: error.message });
+    }
+}
--- src/app/orders/page.js
+++ src/app/orders/page.js
@@ -1,82 +1,81 @@
-'use client'
+'use client';
+import Loading from '@/components/Loading';
+import axios from 'axios';
+import Link from 'next/link';
 import { useRouter } from 'next/navigation';
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react';
+import { toast } from 'react-toastify';
+// import { cookies } from 'next/headers';
 
 const Orders = () => {
   const router = useRouter();
+  const [orders, setOrders] = useState([])
   useEffect(() => {
-      if (!localStorage.getItem('token')) {
+    const fetchOrders = async () => {
+      let { data } = await axios.post(`${process.env.NEXT_PUBLIC_HOST}/api/myorders`);
+      if (!data.success) {
         router.push('/');
+        toast.error(data.message, {
+          position: "top-center",
+          autoClose: 1000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "light",
+        });
       }
-    }, [])
-  return (
-    <div className='container mx-auto px-5'>
-      <h1 className='font-bold text-xl py-10 px-2 text-center'>My Orders</h1>
+      else {
+        setOrders(data.orders);
+      }
+    }
 
-      <div className="relative overflow-x-auto">
+    fetchOrders();
+  }, [])
+  return (
+    <div className='container mx-auto px-5 min-h-[86vh]'>
+      <h1 className='font-bold text-xl py-10 px-2 text-center'>My Orders</h1>
+    {orders.length === 0 && <Loading />}
+      {orders.length > 0 && <div className="relative overflow-x-auto">
         <table className="w-full text-sm text-left text-gray-500">
           <thead className="text-xs text-gray-700 uppercase border-b dark:border-gray-700 bg-gray-50">
             <tr>
               <th scope="col" className="px-6 py-3">
-                Product name
+                Order Id
               </th>
               <th scope="col" className="px-6 py-3">
-                Color
+                Email
               </th>
               <th scope="col" className="px-6 py-3">
-                Category
+                Amount
               </th>
               <th scope="col" className="px-6 py-3">
-                Price
+                Details
               </th>
             </tr>
           </thead>
           <tbody>
-            <tr className="bg-white border-b dark:border-gray-700">
-              <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
-                Apple MacBook Pro 1
-              </th>
-              <td className="px-6 py-4">
-                Silver
-              </td>
-              <td className="px-6 py-4">
-                Laptop
-              </td>
-              <td className="px-6 py-4">
-                $2999
-              </td>
-            </tr>
-            <tr className="bg-white border-b dark:border-gray-700">
-              <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
-                Microsoft Surface Pro
-              </th>
-              <td className="px-6 py-4">
-                White
-              </td>
-              <td className="px-6 py-4">
-                Laptop PC
-              </td>
-              <td className="px-6 py-4">
-                $1999
-              </td>
-            </tr>
-            <tr className="bg-white border-b dark:border-gray-700">
-              <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
-                Magic Mouse 2
-              </th>
-              <td className="px-6 py-4">
-                Black
-              </td>
-              <td className="px-6 py-4">
-                Accessories
-              </td>
-              <td className="px-6 py-4">
-                $99
-              </td>
-            </tr>
+            {orders && orders.map((order, i) => (
+              <tr key={i} className="bg-white border-b dark:border-gray-700">
+                <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
+                  {order.paymentInfo.razorpay_order_id}
+                </th>
+                <td className="px-6 py-4">
+                  {order.email}
+                </td>
+                <td className="px-6 py-4">
+                  {order.checkoutAmount}
+                </td>
+                <td className="px-6 py-4">
+                  <Link href={`/order?id=${order.paymentInfo.razorpay_order_id}`}>Detail</Link>
+                </td>
+              </tr>
+            ))
+            }
           </tbody>
         </table>
-      </div>
+      </div>}
 
     </div>
   )
