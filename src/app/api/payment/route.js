61dda27df8dda46c743be81d406ad25d27548a16
Vivek <vivekparde2411@gmail.com>
2023-09-06 10:51:46 +0530

    solved cart tamparing

--- src/app/api/payment/route.js
+++ src/app/api/payment/route.js
@@ -1,12 +1,23 @@
 import connectToDb from "@/middleware/db";
 import Order from "@/models/Order";
+import Product from "@/models/Product";
 import { NextResponse } from "next/server";
 import Razorpay from "razorpay";
 
 export async function POST(Request) {
     try {
-
         const { email, checkoutAmount, items, address } = await Request.json();
+        let subTotal = 0;
+        for (const item of items) {
+            let product = await Product.findOne({slug: item.slug});
+                subTotal += item.price * item.qty;
+            if (product.price !== item.price) {
+                return NextResponse.json({ success: false, message: 'The price of some products in your cart have changed. Please try again' }, {status: 400});
+            }
+        }
+        if (subTotal !== checkoutAmount) {
+            return NextResponse.json({ success: false, message: 'The price of some products in your cart have changed. Please try again' }, {status:400});
+        }
         await connectToDb();
         const instance = new Razorpay({ key_id: `${process.env.RAZORPAY_KEY_ID}`, key_secret: `${process.env.RAZORPAY_KEY_SECRET}` })
         const order = await instance.orders.create({
@@ -28,6 +39,6 @@ export async function POST(Request) {
         await myorder.save();
         return NextResponse.json({ success: true, order, id: myorder._id });
     } catch (error) {
-        return NextResponse.json({ success: false, message: error.message });
+        return NextResponse.json({ success: false, message: error.message },{status: 500});
     }
 }
