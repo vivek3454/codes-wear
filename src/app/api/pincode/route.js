0309496ea3b547d22d10273eee660cf274a69f3c
Vivek <vivekparde2411@gmail.com>
2023-09-06 10:44:50 +0530

    added city and state

--- src/app/api/pincode/route.js
+++ src/app/api/pincode/route.js
@@ -1,5 +1,11 @@
 import { NextResponse } from "next/server";
 
 export async function GET(Request) {
-  return NextResponse.json([431133, 431103, 431109]);
+  let pincodes = {
+    '431103': ['Sambhajinagar', 'Maharashtra'],
+    '110003': ['Delhi', 'Delhi'],
+    '560017': ['Banglore', 'Karnataka'],
+    '721302': ['Kharagpur', 'West Bengal'],
+  }
+  return NextResponse.json(pincodes);
 }
