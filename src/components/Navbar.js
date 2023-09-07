21f7f85d3122b02b96fdb1b989bd8c9221430d63
Vivek <vivekparde2411@gmail.com>
2023-09-07 23:47:10 +0530

    added hover effect

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,18 +5,18 @@ import { AiOutlineShoppingCart, AiFillCloseCircle, AiOutlinePlusCircle, AiOutlin
 import { BsFillBagCheckFill } from 'react-icons/bs';
 import { MdAccountCircle } from 'react-icons/md';
 import { useDispatch, useSelector } from 'react-redux';
-import { clear, add, remove, subTotal, getProductsCartFromLocalStorage } from '@/redux/CartSlice';
+import { clear, add, remove, subTotal, getCartFromLocalStorage } from '@/redux/CartSlice';
 
 const Navbar = ({ user, handleLogout }) => {
   const dispatch = useDispatch();
   const [isOpen, setIsOpen] = useState(false);
   const [toggleDropDown, setToggleDropDown] = useState(false)
-  const items = useSelector((state) => state.cart.products);
+  const { cart } = useSelector((state) => state.cart);
   const cartRef = useRef();
   const cart1Ref = useRef();
 
   useEffect(() => {
-    dispatch(getProductsCartFromLocalStorage());
+    dispatch(getCartFromLocalStorage());
   }, [])
   const openCart = () => {
     setIsOpen(true);
@@ -55,24 +55,24 @@ const Navbar = ({ user, handleLogout }) => {
 
   return (
     <header className="text-gray-600 sticky top-0 z-50 bg-white shadow-md">
-      <div className="container mx-auto flex justify-between flex-wrap p-2 flex-col md:flex-row items-center">
+      <div className="container mx-auto flex justify-between flex-wrap p-2 flex-col md:flex-row cart-center">
         <Link href={'/'} className="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
           <Image className='-mt-1 -mr-7 -ml-5' width={65} height={10} src="/codesWear.png" alt="logo" />
           <span className="ml-3 text-xl">CodesWear.com</span>
         </Link>
         <nav className="flex flex-wrap items-center text-base justify-center">
-          <Link href={'/tshirts'} className="mr-5 hover:text-gray-900">Tshirts</Link>
-          <Link href={'/hoodies'} className="mr-5 hover:text-gray-900">Hoodies</Link>
-          <Link href={'stickers'} className="mr-5 hover:text-gray-900">Stickers</Link>
-          <Link href={'/mugs'} className="mr-5 hover:text-gray-900">Mugs</Link>
+          <Link href={'/tshirts'} className="mr-5 hover:text-red-500">Tshirts</Link>
+          <Link href={'/hoodies'} className="mr-5 hover:text-red-500">Hoodies</Link>
+          <Link href={'stickers'} className="mr-5 hover:text-red-500">Stickers</Link>
+          <Link href={'/mugs'} className="mr-5 hover:text-red-500">Mugs</Link>
         </nav>
         <div className='flex items-center relative'>
           {user.value &&
-            <Link ref={cart1Ref} href={'/login'} className="mr-4 text-[26px] absolute top-4 right-1 md:static hover:text-gray-900">
+            <div ref={cart1Ref} className="cursor-pointer hover:text-red-500 mr-4 text-[26px] absolute top-4 right-1 md:static">
               <MdAccountCircle onMouseOver={() => setToggleDropDown(true)} onMouseLeave={() => setToggleDropDown(false)} />
-            </Link>
+            </div>
           }
-          {toggleDropDown && <div onMouseOver={() => setToggleDropDown(true)} onMouseLeave={() => setToggleDropDown(false)} className='absolute right-0 top-6 rounded-md px-5 bg-white w-36'>
+          {toggleDropDown && <div onMouseOver={() => setToggleDropDown(true)} onMouseLeave={() => setToggleDropDown(false)} className='absolute right-0 top-8 rounded-md px-5 bg-white w-36 shadow-xl'>
             <ul className='py-2'>
               <Link href={'/myAccount'}><li className='py-1 hover:text-black text-sm'>My Account</li></Link>
               <Link href={'/orders'}><li className='py-1 hover:text-black text-sm'>Orders</li></Link>
@@ -80,9 +80,9 @@ const Navbar = ({ user, handleLogout }) => {
             </ul>
           </div>}
           {!user.value && <Link href={'/login'} className="flex mr-4 w-full justify-center rounded-md bg-red-500 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-red-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-500">Sign in</Link>}
-          <div ref={cart1Ref} onClick={openCart} className="mr-5 cursor-pointer text-2xl absolute top-3 right-1 md:relative md:top-0 hover:text-gray-900">
+          <div ref={cart1Ref} onClick={openCart} className="mr-5 cursor-pointer text-2xl absolute top-3 right-1 md:relative md:top-0 hover:text-red-500">
             <AiOutlineShoppingCart />
-            {items?.length > 0 && <span className='absolute top-2 left-4 text-sm bg-red-500 w-5 h-5 flex justify-center items-center font-semibold rounded-full'>{items?.length}</span>}
+            {cart?.length > 0 && <span className='absolute top-2 left-4 text-sm bg-red-500 text-black w-5 h-5 flex justify-center items-center font-semibold rounded-full'>{cart?.length}</span>}
           </div>
         </div>
       </div>
@@ -90,7 +90,7 @@ const Navbar = ({ user, handleLogout }) => {
         <h2 className='font-bold text-xl text-center'>Shopping Cart</h2>
         <span onClick={closeCart} className='absolute text-2xl cursor-pointer top-2 right-2 text-red-400'><AiFillCloseCircle /></span>
         <ol className='list-decimal font-semibold mb-10'>
-          {items && items?.map((item, index) => (
+          {cart && cart?.map((item, index) => (
             <li key={index}>
               <div className="flex items-center my-3">
                 <div className='w-2/3 font-semibold'>{item.name}<br />({item.size}/{item.color})</div>
@@ -101,9 +101,9 @@ const Navbar = ({ user, handleLogout }) => {
         </ol>
         <div className="flex gap-2">
           <Link href={'/checkout'}>
-            <button disabled={items.length === 0} className="flex text-white disabled:bg-red-300 bg-red-500 border-0 py-1 px-2 focus:outline-none hover:bg-red-600 rounded"><BsFillBagCheckFill className='m-1' /> CheckOut</button>
+            <button disabled={cart.length === 0} className="flex text-white disabled:bg-red-300 bg-red-500 border-0 py-1 px-2 focus:outline-none hover:bg-red-600 rounded"><BsFillBagCheckFill className='m-1' /> CheckOut</button>
           </Link>
-          <button disabled={items.length === 0} onClick={handleClearCart} className="flex text-white disabled:bg-red-300 bg-red-500 border-0 py-1 px-2 focus:outline-none hover:bg-red-600 rounded">Clear Cart</button>
+          <button disabled={cart.length === 0} onClick={handleClearCart} className="flex text-white disabled:bg-red-300 bg-red-500 border-0 py-1 px-2 focus:outline-none hover:bg-red-600 rounded">Clear Cart</button>
         </div>
       </div>
     </header>
