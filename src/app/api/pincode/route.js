8de90e712f4a08023dc2ab3e5e782a2842fb26fe
Vivek <vivekparde2411@gmail.com>
2023-09-09 20:10:18 +0530

    added pincodes in json

--- src/app/api/pincode/route.js
+++ src/app/api/pincode/route.js
@@ -1,12 +1,6 @@
 import { NextResponse } from "next/server";
+import pincodes from "../../../data/pincodes";
 
 export async function GET(Request) {
-  // pincodes object with key: pincode and value:array of district and state
-  let pincodes = {
-    '431103': ['Sambhajinagar', 'Maharashtra'],
-    '110003': ['Delhi', 'Delhi'],
-    '560017': ['Banglore', 'Karnataka'],
-    '721302': ['Kharagpur', 'West Bengal'],
-  }
   return NextResponse.json(pincodes);
 }
