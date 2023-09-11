df6496d88bebd5744d952cdda9c56e73c90cae9f
Vivek <vivekparde2411@gmail.com>
2023-09-11 19:00:37 +0530

    styled product cards

--- src/app/hoodies/page.js
+++ src/app/hoodies/page.js
@@ -1,49 +1,61 @@
-'use client'
-import Loading from '@/components/Loading';
-import axios from 'axios';
-import Link from 'next/link'
-import React, { useEffect, useState } from 'react'
+"use client";
+import Loading from "@/components/Loading";
+import axios from "axios";
+import Link from "next/link";
+import React, { useEffect, useState } from "react";
 
 const Hoodies = () => {
   const [hoodies, setHoodies] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
   // getting all hoodies and storing in state
   useEffect(() => {
     const getAllhoodies = async () => {
-      const res = await axios.get(`${process.env.NEXT_PUBLIC_HOST}/api/getHoodies`)
+      const res = await axios.get(`${process.env.NEXT_PUBLIC_HOST}/api/getHoodies`);
       setHoodies(res.data.hoodies);
-    }
+      setIsLoading(false);
+    };
     getAllhoodies();
-  }, [])
+  }, []);
 
   return (
     <section className="text-gray-600 container mx-auto">
       <div className="px-5 py-24 mx-auto">
-      {Object.keys(hoodies).length === 0 && <Loading />}
         <div className="flex flex-wrap justify-center -m-4">
+          {Object.keys(hoodies).length === 0 && isLoading && <Loading />}
+          {Object.keys(hoodies).length === 0 && !isLoading &&
+            <div className="text-xl font-semibold h-[70vh] flex justify-center items-center">
+              Currently hoodies are not available
+            </div>}
           {Object.keys(hoodies).map((product, i) => (
 
-            <Link key={i} href={`/product/${hoodies[product].slug}`} className='shadow m-2 rounded'>
-              <div className="w-60 m-5 p-4">
+            <Link key={i} href={`/product/${hoodies[product].slug}`} className="shadow-xl hover:shadow m-2 rounded">
+              <div className="w-60 m-2 p-2">
                 <div className="block relative rounded overflow-hidden">
-                  <img alt="ecommerce" className="object-contain object-center w-full h-[45vh] block" src="https://m.media-amazon.com/images/I/61BYaul0M6L._UY741_.jpg" />
+                  <img alt="ecommerce" className="object-contain object-center w-full h-[35vh] block" src={hoodies[product].img} />
                 </div>
                 <div className="mt-4">
-                  <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">T-Shirts</h3>
-                  <h2 className="text-gray-900 title-font text-lg font-medium">{hoodies[product].title}</h2>
-                  <p className="mt-1">₹{hoodies[product].price}</p>
-                  <div className="mt-1">
-                    {hoodies[product].size.includes('S') && <span className='border border-gray-400 mx-1 px-1'>S</span>}
-                    {hoodies[product].size.includes('M') && <span className='border border-gray-400 mx-1 px-1'>M</span>}
-                    {hoodies[product].size.includes('L') && <span className='border border-gray-400 mx-1 px-1'>L</span>}
-                    {hoodies[product].size.includes('XL') && <span className='border border-gray-400 mx-1 px-1'>XL</span>}
-                    {hoodies[product].size.includes('XXL') && <span className='border border-gray-400 mx-1 px-1'>XXL</span>}
+                  <div className="flex justify-between items-center">
+                    <h2 className="text-gray-900 title-font text-lg font-bold">{hoodies[product].title}</h2>
+                    <h3 className="text-gray-500 text-xs tracking-widest font-bold title-font mb-1">Tshirt</h3>
                   </div>
-                  <div className="mt-1">
-                    {hoodies[product].color.includes('red') && <button className="border-2 border-gray-300 ml-1 bg-red-500 rounded-full w-6 h-6 focus:outline-none"></button>}
-                    {hoodies[product].color.includes('blue') && <button className="border-2 border-gray-300 ml-1 bg-blue-500 rounded-full w-6 h-6 focus:outline-none"></button>}
-                    {hoodies[product].color.includes('black') && <button className="border-2 border-gray-300 ml-1 bg-black rounded-full w-6 h-6 focus:outline-none"></button>}
-                    {hoodies[product].color.includes('green') && <button className="border-2 border-gray-300 ml-1 bg-green-500 rounded-full w-6 h-6 focus:outline-none"></button>}
-                    {hoodies[product].color.includes('orange') && <button className="border-2 border-gray-300 ml-1 bg-orange-500 rounded-full w-6 h-6 focus:outline-none"></button>}
+                  <div className="flex justify-between">
+                    <div>
+                      <div className="mt-1 flex gap-1">
+                        {hoodies[product].color.includes("red") && <button className="border-2 border-gray-300 bg-red-500 rounded-full w-4 h-4 focus:outline-none"></button>}
+                        {hoodies[product].color.includes("blue") && <button className="border-2 border-gray-300 bg-blue-500 rounded-full w-4 h-4 focus:outline-none"></button>}
+                        {hoodies[product].color.includes("black") && <button className="border-2 border-gray-300 bg-black rounded-full w-4 h-4 focus:outline-none"></button>}
+                        {hoodies[product].color.includes("green") && <button className="border-2 border-gray-300 bg-green-500 rounded-full w-4 h-4 focus:outline-none"></button>}
+                        {hoodies[product].color.includes("orange") && <button className="border-2 border-gray-300 bg-orange-500 rounded-full w-4 h-4 focus:outline-none"></button>}
+                      </div>
+                      <div className="mt-3">
+                        {hoodies[product].size.includes("S") && <span className="text-sm border border-gray-400 mr-1 px-1 py-1">S</span>}
+                        {hoodies[product].size.includes("M") && <span className="text-sm border border-gray-400 mx-1 px-1 py-1">M</span>}
+                        {hoodies[product].size.includes("L") && <span className="text-sm border border-gray-400 mx-1 px-1 py-1">L</span>}
+                        {hoodies[product].size.includes("XL") && <span className="text-sm border border-gray-400 mx-1 px-1 py-1">XL</span>}
+                        {hoodies[product].size.includes("XXL") && <span className="text-sm border border-gray-400 mx-1 px-1 py-1">XXL</span>}
+                      </div>
+                    </div>
+                    <p className="mt-1">₹{hoodies[product].price}</p>
                   </div>
                 </div>
               </div>
@@ -52,7 +64,7 @@ const Hoodies = () => {
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Hoodies
+export default Hoodies;
--- src/app/tshirts/page.js
+++ src/app/tshirts/page.js
@@ -1,49 +1,65 @@
-'use client'
-import Loading from '@/components/Loading';
-import Link from 'next/link'
-import React, { useEffect, useState } from 'react'
+"use client";
+import Loading from "@/components/Loading";
+import Head from "next/head";
+import Link from "next/link";
+import React, { useEffect, useState } from "react";
 
 const Tshirts = () => {
   const [tshirts, setTshirts] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
   // getting all tshirts and storing in state
   useEffect(() => {
     const getAlltshirts = async () => {
-      const res = await fetch(`${process.env.NEXT_PUBLIC_HOST}/api/getTshirts`)
+      const res = await fetch(`${process.env.NEXT_PUBLIC_HOST}/api/getTshirts`);
       const data = await res.json();
       setTshirts(data.tshirts);
-    }
+      setIsLoading(false);
+    };
     getAlltshirts();
-  }, [])
+  }, []);
 
   return (
     <section className="text-gray-600 container mx-auto">
-      <div className="px-5 py-24 mx-auto">
-        {Object.keys(tshirts).length === 0 && <Loading />}
+      <Head>
+        <title>Tshirt - CodesWear.com</title>
+      </Head>
+      <div className="px-5 py-24  mx-auto">
+        {Object.keys(tshirts).length === 0 && isLoading && <Loading />}
+        {Object.keys(tshirts).length === 0 && !isLoading &&
+          <div className="text-xl font-semibold h-[70vh] flex justify-center items-center">
+            Currently tshirts are not available
+          </div>}
         {Object.keys(tshirts).length > 0 && <div className="flex flex-wrap justify-center -m-4">
           {Object.keys(tshirts).map((product, i) => (
 
-            <Link key={i} href={`/product/${tshirts[product].slug}`} className='shadow m-2 rounded'>
-              <div className="w-60 m-5 p-4">
+            <Link key={i} href={`/product/${tshirts[product].slug}`} className="shadow-xl hover:shadow m-2 rounded">
+              <div className="w-60 m-2 p-2">
                 <div className="block relative rounded overflow-hidden">
-                  <img alt="ecommerce" className="object-contain object-center w-full h-[45vh] block" src="https://m.media-amazon.com/images/I/71tJWaahoDL._UY741_.jpg" />
+                  <img alt="ecommerce" className="object-contain object-center w-full h-[35vh] block" src={tshirts[product].img} />
                 </div>
                 <div className="mt-4">
-                  <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">T-Shirts</h3>
-                  <h2 className="text-gray-900 title-font text-lg font-medium">{tshirts[product].title}</h2>
-                  <p className="mt-1">₹{tshirts[product].price}</p>
-                  <div className="mt-1">
-                    {tshirts[product].size.includes('S') && <span className='border border-gray-400 mx-1 px-1'>S</span>}
-                    {tshirts[product].size.includes('M') && <span className='border border-gray-400 mx-1 px-1'>M</span>}
-                    {tshirts[product].size.includes('L') && <span className='border border-gray-400 mx-1 px-1'>L</span>}
-                    {tshirts[product].size.includes('XL') && <span className='border border-gray-400 mx-1 px-1'>XL</span>}
-                    {tshirts[product].size.includes('XXL') && <span className='border border-gray-400 mx-1 px-1'>XXL</span>}
+                  <div className="flex justify-between items-center">
+                    <h2 className="text-gray-900 title-font text-lg font-bold">{tshirts[product].title}</h2>
+                    <h3 className="text-gray-500 text-xs tracking-widest font-bold title-font mb-1">Tshirt</h3>
                   </div>
-                  <div className="mt-1">
-                    {tshirts[product].color.includes('red') && <button className="border-2 border-gray-300 ml-1 bg-red-500 rounded-full w-6 h-6 focus:outline-none"></button>}
-                    {tshirts[product].color.includes('blue') && <button className="border-2 border-gray-300 ml-1 bg-blue-500 rounded-full w-6 h-6 focus:outline-none"></button>}
-                    {tshirts[product].color.includes('black') && <button className="border-2 border-gray-300 ml-1 bg-black rounded-full w-6 h-6 focus:outline-none"></button>}
-                    {tshirts[product].color.includes('green') && <button className="border-2 border-gray-300 ml-1 bg-green-500 rounded-full w-6 h-6 focus:outline-none"></button>}
-                    {tshirts[product].color.includes('orange') && <button className="border-2 border-gray-300 ml-1 bg-orange-500 rounded-full w-6 h-6 focus:outline-none"></button>}
+                  <div className="flex justify-between">
+                    <div>
+                      <div className="mt-1 flex gap-1">
+                        {tshirts[product].color.includes("red") && <button className="border-2 border-gray-300 bg-red-500 rounded-full w-4 h-4 focus:outline-none"></button>}
+                        {tshirts[product].color.includes("blue") && <button className="border-2 border-gray-300 bg-blue-500 rounded-full w-4 h-4 focus:outline-none"></button>}
+                        {tshirts[product].color.includes("black") && <button className="border-2 border-gray-300 bg-black rounded-full w-4 h-4 focus:outline-none"></button>}
+                        {tshirts[product].color.includes("green") && <button className="border-2 border-gray-300 bg-green-500 rounded-full w-4 h-4 focus:outline-none"></button>}
+                        {tshirts[product].color.includes("orange") && <button className="border-2 border-gray-300 bg-orange-500 rounded-full w-4 h-4 focus:outline-none"></button>}
+                      </div>
+                      <div className="mt-3">
+                        {tshirts[product].size.includes("S") && <span className="text-sm border border-gray-400 mr-1 px-1 py-1">S</span>}
+                        {tshirts[product].size.includes("M") && <span className="text-sm border border-gray-400 mx-1 px-1 py-1">M</span>}
+                        {tshirts[product].size.includes("L") && <span className="text-sm border border-gray-400 mx-1 px-1 py-1">L</span>}
+                        {tshirts[product].size.includes("XL") && <span className="text-sm border border-gray-400 mx-1 px-1 py-1">XL</span>}
+                        {tshirts[product].size.includes("XXL") && <span className="text-sm border border-gray-400 mx-1 px-1 py-1">XXL</span>}
+                      </div>
+                    </div>
+                    <p className="mt-1">₹{tshirts[product].price}</p>
                   </div>
                 </div>
               </div>
@@ -52,7 +68,7 @@ const Tshirts = () => {
         </div>}
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Tshirts
+export default Tshirts;
