12fb9c6084f8149788adb29a527da619bfca52b3
Vivek <vivekparde2411@gmail.com>
2023-09-20 14:01:45 +0530

    added loader

---
+++ src/app/pages/product/tshirts/loading.js
@@ -0,0 +1,12 @@
+import Loading from "@/components/Loading";
+import React from "react";
+
+const Page = () => {
+    return (
+        <div className="py-14">
+            <Loading />
+        </div>
+    );
+};
+
+export default Page;
